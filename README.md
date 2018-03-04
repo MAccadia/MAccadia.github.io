0089f1e18fe6dc1f530de91a8b7e831f7cb8e09e
Maxime ACCADIA <maxime.accadia@gmail.com>
2018-03-04 21:12:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# An interactive map with a picture & leafletjs
+
+Based on : [Using leaflet.js to pan and zoom a big image](https://gist.github.com/longhotsummer/ba9c96bb2abb304e4095ce00df17ae2f)
+
+## Adding a markers
+
+1. Get the position of your marker with right click on the map.
+
+2. Edit markers.js to add your marker.
---
+++ markers.js
@@ -0,0 +1,11 @@
+var markers = [
+{
+  lat: -77.4,
+  lng: 99.8,
+  popup: "<p>Rucher</p><p><a href=\"images/rucher_full.jpg\"><img src=\"images/rucher.jpg\" width=100%/></a></p>"
+},
+{
+  lat: -99.7,
+  lng: 506.6,
+  popup: "<p>Artichaud</p><a href=\"images/artichaud_full.jpg\"><img src=\"images/artichaud.jpg\" width=100%/></a></p>"
+}];
