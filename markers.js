da25cd3723b0e9150db2f3ffbaec211d1d0655aa
Maxime ACCADIA <maxime.accadia@gmail.com>
2018-03-05 21:27:17 +0100

    Marker layers

--- markers.js
+++ markers.js
@@ -1,11 +1,23 @@
-var markers = [
+var places = [
 {
   lat: -77.4,
   lng: 99.8,
   popup: "<p>Rucher</p><p><a href=\"images/rucher_full.jpg\"><img src=\"images/rucher.jpg\" width=100%/></a></p>"
 },
 {
-  lat: -99.7,
-  lng: 506.6,
+  lat: -105,
+  lng: 307,
+  popup: "<p>Terrasse</p>"
+}];
+
+var plants = [
+{
+  lat: -230,
+  lng: 491,
+  popup: "<p>Figuier</p>"
+},
+{
+  lat: -105,
+  lng: 174,
   popup: "<p>Artichaud</p><a href=\"images/artichaud_full.jpg\"><img src=\"images/artichaud.jpg\" width=100%/></a></p>"
 }];
